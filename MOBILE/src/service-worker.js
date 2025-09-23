1045dfede8ea809d0cde7e9b7a59055b4554680a
Erick Felipe <erickfelipe5424@gmail.com>
2025-09-23 12:55:37 -0300

    Update service-worker.js

--- MOBILE/src/service-worker.js
+++ MOBILE/src/service-worker.js
@@ -1,21 +1,22 @@
 self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open("Relogio-Motivacional-Digital-cache").then(cache => {
-        return cache.addAll([
-            "index.html",
-            "manifest.json",
-            "manha.jpg",
-            "tarde.jpg",
-            "noite.jpg",
-            "madrugada.jpg",
-            "icon-192.png",
-            "icon-512.png",
+  event.waitUntil(
+    caches.open("Relogio-Motivacional-Digital-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icone-192.png",
+        "icon-512.png",
+        "manhã1.jpg",
+        "tarde.jpg",
+        "noite.jpg",
+        "madrugada.jpg"
       ]);
     })
   );
 });
+
 self.addEventListener("fetch", event => {
-    event.respondWith(
-     caches.match(event.request).then(response => response || fetch (event.request))
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
